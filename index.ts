4bb34de95418f13dc96678a72ff834eeafb66954
Vladyslav Hrabovyii <58665842+gwynplaine666@users.noreply.github.com>
2025-12-06 21:26:39 +0000

    day 1: reogranize

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
-import type { Rotation } from "./challenges/day-1/rotation.type";
-import { SecretEntrance } from "./challenges/day-1/secret-entrance";
+import type { Rotation } from "./day-1/rotation.type";
+import { SecretEntrance } from "./day-1/secret-entrance";
 
 const rotations = await Bun.file(`${import.meta.dir}/challenges/day-1/puzzle-input.txt`).text().then(text => text.split("\n").map(line => line.trim()).filter(line => line.length > 0)) as Rotation[]
 const SecretEntranceProblem = new SecretEntrance(rotations)
